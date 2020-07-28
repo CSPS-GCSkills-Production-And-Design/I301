8138e8a8537aeec165848131ae9d3a3a65a35b7f
Antoine-Gagnon <62062397+Antoine-Gagnon@users.noreply.github.com>
2020-07-28 08:03:25 -0400

    I301

---
+++ Dev/settings/settings-general.js
@@ -0,0 +1,23 @@
+'use strict';
+
+define({
+
+
+	/*------------------ Course info -----------------*/
+	courseLegacyCode:"I301",
+	courseTitle_en: "Fundamentals of Information Management",
+	courseTitle_fr: "Principes fondamentaux de la gestion de l’information",
+	courseSubtitle_en: "",
+	courseSubtitle_fr: "",
+	seriesTitle_en: 	 "",
+	seriesTitle_fr: 	 "",
+
+	showSitemap: true,
+
+	showGlossary: true,
+
+	trackAllPages: true,
+	triggerCompletionWhenAllPagesViewed: false,
+	
+	googleAnalyticsID: "UA-101907595-16"
+});
